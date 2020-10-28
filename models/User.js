2945ced80ea625f45127cc04624268936edffcb3
ASC8 <asamcent@gmail.com>
2020-10-28 16:19:02 -0700

    get, post, put and delete routes written

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create a User Model
+class User extends Model {}
+
+// Define table columns and configuration
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS GO HERE
+        // define id column 
+        id: {
+            // use the special Sequelize datatypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's 'NOT NULL' option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        // define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // Define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // There cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to fals, we can run our data through validatiors before creating the table
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // this means the password must be at least four characters  lon
+                len: [4]
+            }
+        }
+    },
+    {
+        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        // dont automaticall create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // dont pluralize name of databse table
+        freezeTableName: true,
+        // use underscores instead of camel-casing ('i.e comment_text` and not `commentText`)
+        underscored: true,
+        // Make it so our model name stays lowercase in the database
+        modelName: `user`
+    }
+);
+
+module.exports = User;
