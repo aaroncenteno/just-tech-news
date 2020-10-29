b2ae7e37ccf87cab9c72cad7cdc4bc75eafea712
ASC8 <asamcent@gmail.com>
2020-10-28 18:58:51 -0700

    bcrypt added

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+const bcrypt = require('bcrypt');
 // create a User Model
 class User extends Model {}
 
@@ -43,11 +43,18 @@ User.init(
                 // this means the password must be at least four characters  lon
                 len: [4]
             }
-        }
+        },
     },
     {
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            beforeCreate(userData) {
+                return bcrypt.hash(userData.password, 10).then(newUserData => {
+                    return newUserData
+                });
+            }
+        },
         // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
         // dont automaticall create createdAt/updatedAt timestamp fields
