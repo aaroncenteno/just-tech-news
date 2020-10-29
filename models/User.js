347d3131a6059c998e811c298fae7f8d30a58c5d
ASC8 <asamcent@gmail.com>
2020-10-28 19:27:37 -0700

    passcode encrypted and verified login

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 // create a User Model
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // Define table columns and configuration
 User.init(
